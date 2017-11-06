5b7930686e41fde7576d03b42887ae301182e5f0
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-06 15:31:10 -0700

    define set-up and tear-down methods

--- test/com/google/gwt/sample/stockwatcher/client/StockWatcherTest.java
+++ test/com/google/gwt/sample/stockwatcher/client/StockWatcherTest.java
@@ -1,6 +1,9 @@
 package com.google.gwt.sample.stockwatcher.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.junit.client.GWTTestCase;
+import com.google.gwt.user.client.ui.Button;
 
 /**
  * GWT JUnit tests must extend GWTTestCase.
@@ -10,9 +13,29 @@ public class StockWatcherTest extends GWTTestCase {
   /**
    * Must refer to a valid module that sources this class.
    */
+	
+	StockWatcher sw;
   public String getModuleName() {
     return "com.google.gwt.sample.stockwatcher.StockWatcher";
   }
+  
+  @Override
+	protected void gwtSetUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.gwtSetUp();
+		
+		sw = new StockWatcher();
+		sw.onModuleLoad();
+		
+	}
+  
+  @Override
+	protected void gwtTearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.gwtTearDown();
+		
+		sw = null;
+	}
 
   /**
    * Add as many tests as you like.
@@ -20,5 +43,8 @@ public class StockWatcherTest extends GWTTestCase {
   public void testSimple() {
     assertTrue(true);
   }
+  
+  
+  
 
 }
