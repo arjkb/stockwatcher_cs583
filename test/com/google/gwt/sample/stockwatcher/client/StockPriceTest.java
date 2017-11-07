c887601935028245b7f511304b3de2adc8444024
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-06 19:14:53 -0700

    test class to test StockPrice class

---
+++ test/com/google/gwt/sample/stockwatcher/client/StockPriceTest.java
@@ -0,0 +1,17 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class StockPriceTest extends GWTTestCase {
+
+	@Override
+	public String getModuleName() {
+		// TODO Auto-generated method stub
+		return "com.google.gwt.sample.stockwatcher.StockWatcher";
+	}
+	
+	public void testSimple()	{
+		assertTrue(true);
+	}
+
+}
