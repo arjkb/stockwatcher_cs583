ea21cabc5fd9718fef223db74a12a9d19edfe487
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-06 19:28:59 -0700

    positive test case to test the change percent of StockPrice

--- test/com/google/gwt/sample/stockwatcher/client/StockPriceTest.java
+++ test/com/google/gwt/sample/stockwatcher/client/StockPriceTest.java
@@ -22,5 +22,22 @@ public class StockPriceTest extends GWTTestCase {
 		assertEquals(10.0, sp.getPrice(), 0);
 		assertEquals(2.0, sp.getChange(), 0);
 	}
+	
+	// positive test case -- Arjun Krishna Babu
+	public void testChangePercent()	{
+		StockPrice sp = new StockPrice();
+		
+		sp.setPrice(10.0);
+		sp.setChange(2.0);
+		assertEquals(20.0, sp.getChangePercent(), 0);
+		
+		sp.setPrice(60.0);
+		sp.setChange(15.0);
+		assertEquals(25.0, sp.getChangePercent(), 0);
+		
+		sp.setPrice(10.0);
+		sp.setChange(-5.0);
+		assertEquals(-50.0, sp.getChangePercent(), 0);
+	}
 
 }
